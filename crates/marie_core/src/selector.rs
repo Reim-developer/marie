f0d577df8f82c24554860600f29202189f381191
Reim-developer <contact.kaxtr@gmail.com>
2026-08-23 11:54:40 +0700

    feat(selector): implement HtmlSelector for element querying
    Add `HtmlSelector` to provide a high-level abstraction for interacting
    with parsed HTML documents. This includes methods for checking element
    existence, retrieving specific elements via CSS selectors, and
    convenience methods for text extraction.

---
+++ crates/marie_core/src/selector.rs
@@ -0,0 +1,74 @@
+use scraper::{ElementRef, Html, Selector, element_ref::Text};
+
+pub struct HtmlSelector<'a> {
+    pub selector: Selector,
+    pub document: &'a Html,
+}
+
+impl<'a> HtmlSelector<'a> {
+    #[must_use]
+    pub const fn new(selector: Selector, document: &'a Html) -> Self {
+        Self { selector, document }
+    }
+
+    #[must_use]
+    pub fn has(&self) -> bool {
+        self.document.select(&self.selector).next().is_some()
+    }
+
+    #[must_use]
+    pub fn find(&self) -> Option<ElementRef<'a>> {
+        self.document.select(&self.selector).next()
+    }
+
+    #[must_use]
+    pub fn text_element(text: Text) -> String {
+        text.collect::<String>()
+    }
+}
+
+#[test]
+fn test_has_element() {
+    use crate::scraper::Scraper;
+
+    const HTML: &str = "
+        <div> <p> Hello Earth ! <p> <div>
+    ";
+
+    let html = Scraper::new(HTML.to_string()).parse_html();
+    let selector = Scraper::selector("html", &html).unwrap();
+    let selector_2 = Scraper::selector("div.z", &html).unwrap();
+
+    assert!(selector.has());
+    assert!(!selector_2.has());
+}
+
+#[test]
+fn test_element() {
+    use crate::scraper::Scraper;
+
+    const HTML: &str = "
+        <div> <p> Hello Earth ! <p> <div>
+    ";
+
+    let html = Scraper::new(HTML.to_string()).parse_html();
+    let selector = Scraper::selector("div", &html).unwrap();
+
+    assert!(selector.find().is_some());
+    assert_eq!(selector.find().unwrap().value().name(), "div");
+}
+
+#[test]
+fn test_text_element() {
+    use crate::scraper::Scraper;
+
+    const HTML: &str = "
+        <div> <p>Hello Earth !<p> <div>
+    ";
+
+    let html = Scraper::new(HTML.to_string()).parse_html();
+    let selector = Scraper::selector("div p", &html).unwrap();
+    let element = selector.find().unwrap();
+
+    assert_eq!(HtmlSelector::text_element(element.text()), "Hello Earth !");
+}
