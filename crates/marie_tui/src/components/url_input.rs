27a429eb9021cd086add02104ebf9e3a434ae26a
Reim-developer <contact.kaxtr@gmail.com>
2026-08-24 14:36:51 +0700

    feat(ui): implement interactive input handling for url_input
    Add `input_handle` to process keyboard events (Char, Backspace, Enter,
    Esc) for the URL input component. Enhance the `render` method to
    include a visual cursor position and dynamic border styling based on
    input state.

--- crates/marie_tui/src/components/url_input.rs
+++ crates/marie_tui/src/components/url_input.rs
@@ -1,7 +1,10 @@
+use crossterm::event::KeyCode;
 use ratatui::{
     Frame,
-    layout::Rect,
-    widgets::{Block, Borders, Paragraph},
+    layout::{Position, Rect},
+    style::{Color, Style},
+    text::{Line, Span},
+    widgets::{Block, Paragraph},
 };
 
 #[derive(Default)]
@@ -9,11 +12,64 @@ pub struct UrlInput {
     pub url: String,
 }
 
-impl UrlInput {
-    pub fn render(&self, frame: &mut Frame, area: Rect) {
-        let input = Paragraph::new(self.url.as_str())
-            .block(Block::bordered().title("URL").borders(Borders::ALL));
+type SpanV<'a> = [Span<'a>];
+const MARKER: &str = ">  ";
 
-        frame.render_widget(input, area);
+impl UrlInput {
+    pub fn input_handle(&mut self, key: KeyCode) -> bool {
+        type K = KeyCode;
+
+        match key {
+            K::Enter => self.url.clear(),
+            K::Char(c) => self.url.push(c),
+            K::Backspace => {
+                self.url.pop();
+            }
+            K::Esc => return true,
+            _ => {}
+        }
+
+        false
+    }
+
+    fn render_pointer(&self, area: Rect, frame: &mut Frame) {
+        let chars_count = self.url.chars().count();
+        let text_length = u16::try_from(chars_count + 4).unwrap_or(u16::MAX);
+        let cursor_x = area.x + text_length;
+        let cursor_y = area.y + 1;
+
+        frame.set_cursor_position(Position::new(cursor_x, cursor_y));
+    }
+
+    fn block_style(&self) -> Style {
+        if self.url.is_empty() {
+            Style::default()
+        } else {
+            Style::default().fg(Color::Gray)
+        }
+    }
+
+    /// # Errors
+    /// Handle event or rendering TUI component failed.
+    pub fn render(
+        &self,
+        frame: &mut Frame,
+        area: &Rect,
+    ) -> Result<(), anyhow::Error> {
+        type P<'a> = Paragraph<'a>;
+        type S<'a> = Span<'a>;
+        type L<'a> = Line<'a>;
+        type B<'a> = Block<'a>;
+
+        let raw_span: &SpanV = &[S::raw(MARKER), S::raw(self.url.as_str())];
+        let input_border = B::bordered().title("URL");
+        let input_line = L::from(raw_span);
+        let input = P::new(input_line)
+            .block(input_border.border_style(self.block_style()));
+
+        frame.render_widget(input, *area);
+        self.render_pointer(*area, frame);
+
+        Ok(())
     }
 }
