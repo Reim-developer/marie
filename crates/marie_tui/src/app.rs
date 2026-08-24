a4719e92e929b6af77744ef3dc3e55ca094380f8
Reim-developer <contact.kaxtr@gmail.com>
2026-08-24 14:35:08 +0700

    feat(marie_tui): update app rendering logic and layout
    Refactor the `App::render` method to use predefined constraints and
    support multiple components. Added `DownloadButton` to the `App`
    struct and integrated `render_simple_help` into the rendering loop.
    The `render` method now returns a `Result` to handle potential errors
    during the TUI rendering process.

--- crates/marie_tui/src/app.rs
+++ crates/marie_tui/src/app.rs
@@ -1,20 +1,43 @@
-use ratatui::layout::{Constraint, Direction};
+use std::rc::Rc;
+
+use ratatui::layout::{Constraint, Direction, Rect};
 use ratatui::{Frame, layout::Layout};
 
+use crate::components::downloade_button::DownloadButton;
+use crate::components::simple_help::render_simple_help;
 use crate::components::url_input::UrlInput;
 
 #[derive(Default)]
 pub struct App {
     pub url_input: UrlInput,
+    pub download_button: DownloadButton,
 }
 
+type C = Constraint;
 impl App {
-    pub fn render(&self, frame: &mut Frame) {
+    const CONSTRAINTS: [C; 3] = [C::Min(1), C::Length(3), C::Length(1)];
+    const INPUT_CONSTRAINTS: [C; 2] = [C::Min(1), C::Length(12)];
+
+    fn url_input_layout(layout: &Rc<[Rect]>) -> Rc<[Rect]> {
+        Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(Self::INPUT_CONSTRAINTS)
+            .split(layout[1])
+    }
+
+    /// # Errors
+    /// Render TUI application failed.
+    pub fn render(&self, frame: &mut Frame) -> Result<(), anyhow::Error> {
         let layout = Layout::default()
             .direction(Direction::Vertical)
-            .constraints([Constraint::Min(1), Constraint::Length(3)])
+            .constraints(Self::CONSTRAINTS)
             .split(frame.area());
 
-        self.url_input.render(frame, layout[1]);
+        let input_layout = Self::url_input_layout(&layout);
+        self.url_input.render(frame, &input_layout[0])?;
+        self.download_button.render(frame, &input_layout[1])?;
+
+        render_simple_help(frame, &layout[2]);
+        Ok(())
     }
 }
