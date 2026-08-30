67b0a6d063c023b8339b9339cb4fb318347b89d0
Reim-developer <contact.kaxtr@gmail.com>
2026-08-30 15:06:21 +0700

    refactor(focus): implement vertical navigation between UI components
    Add `up` and `down` methods to the `Focus` enum to support vertical
    movement. This enables switching between `FeaturesTable`, `UrlInput`,
    and `DownloadButton` using arrow keys.

--- crates/marie_tui/src/focus.rs
+++ crates/marie_tui/src/focus.rs
@@ -1,5 +1,7 @@
 use crossterm::event::KeyCode;
 
+use crate::focus::Focus::FeaturesTable;
+
 #[derive(Default, PartialEq, Eq)]
 pub enum Focus {
     #[default]
@@ -13,6 +15,8 @@ impl Focus {
         match key {
             KeyCode::Left => self.left(),
             KeyCode::Right => self.right(),
+            KeyCode::Up => self.up(),
+            KeyCode::Down => self.down(),
             _ => {}
         }
     }
@@ -23,5 +27,19 @@ impl Focus {
 
     pub const fn right(&mut self) {
         *self = Self::DownloadButton;
+    }
+
+    pub const fn up(&mut self) {
+        *self = match self {
+            Self::FeaturesTable => Self::UrlInput,
+            Self::UrlInput | Self::DownloadButton => FeaturesTable,
+        }
+    }
+
+    pub const fn down(&mut self) {
+        *self = match self {
+            Self::FeaturesTable => Self::UrlInput,
+            Self::UrlInput | Self::DownloadButton => Self::FeaturesTable,
+        }
     }
 }
