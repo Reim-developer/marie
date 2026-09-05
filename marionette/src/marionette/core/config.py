6c2e2c51e95006f2635f4ef61e81ea21af33087a
Reim-developer <contact.kaxtr@gmail.com>
2026-09-05 10:57:07 +0700

    refactor(config): introduce Html dataclass for output type specification
    Replace the string-based `type` field in `Output` with a structured
    `Html` dataclass to allow for more granular configuration of
    output properties, such as image counts. Added a `new_config`
    factory function to streamline `MarionetteConfig` instantiation.

--- marionette/src/marionette/core/config.py
+++ marionette/src/marionette/core/config.py
@@ -9,13 +9,22 @@ class Project:
 
 
 @dataclass
+class Html:
+	images: int
+
+
+@dataclass
 class Output:
 	to: LiteralString
 	dir: str
-	type: str
+	type: Html
 
 
 @dataclass
 class MarionetteConfig:
 	project: Project
 	output_type: Output
+
+
+def new_config(project: Project, output_type: Output) -> MarionetteConfig:
+	return MarionetteConfig(project=project, output_type=output_type)
