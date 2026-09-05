a094dda025e5dbc0040229aa3ec663621bd50231
Reim-developer <contact.kaxtr@gmail.com>
2026-09-05 15:40:29 +0700

    feat(cli): add generate command to main entry point

--- marionette/src/marionette/cli/main_cli.py
+++ marionette/src/marionette/cli/main_cli.py
@@ -1,5 +1,6 @@
 from typer import Typer
 from marionette.cli.commands.init import init_command
+from marionette.cli.commands.generate import generate_command
 
 
 class MarionetteCli:
@@ -7,6 +8,7 @@ class MarionetteCli:
 		self.app = Typer(pretty_exceptions_enable=False, rich_markup_mode=None)
 
 		self.app.add_typer(init_command)
+		self.app.add_typer(generate_command)
 
 	def __call__(self) -> None:
 		self.app()
